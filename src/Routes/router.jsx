2022882f0ef31a4c69dec723af643cd6a0157afa
Tanvirul islam <tanveer99ryan@gmail.com>
2025-03-04 03:57:28 +0600

    header component add

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Root from "../layouts/Root";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,68 @@
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/al-review"}>All Reviews</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/add-review"}>Add Review</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/my-reviews"}>My Reviews</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/game-watchList"}>Game WatchList</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className=" bg-black">
+      <div className="text-white navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-black rounded-box mt-3 w-52 p-2"
+            >
+              <div className=" rounded font-bold z-10 transition duration-500 ease-in-out hover:bg-[#00D283]  ">
+                {links}
+              </div>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl md:text-2xl font-bold">
+            Chill Gamer
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <button className="btn">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
