e264e3c7fbb43ff74194fd8d3f0fae4a6be59e2b
Tanvirul islam <tanveer99ryan@gmail.com>
2025-03-04 04:04:24 +0600

    error component add

---
+++ src/Pages/Error/Error.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+const Error = () => {
+    return (
+        <div className="w-4xl mx-auto mt-12 text-center">
+            <h1 className="text-7xl font-extrabold text-red-600">OOps!!!</h1>
+            <h3 className="text-3xl font-bold my-10">Not Found</h3>
+            <p>Go to <Link to={'/'}><span className="underline font-bold">Home</span></Link></p>
+        </div>
+    );
+};
+
+export default Error;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Root from "../layouts/Root";
+import Error from "../Pages/Error/Error";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement: <Error></Error>,
       children: [
         {
             path: '/',
