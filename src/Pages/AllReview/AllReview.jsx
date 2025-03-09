1404f9fbfdf07b92e8db5eda17d32c45441c4812
Tanvirul islam <tanveer99ryan@gmail.com>
2025-03-09 16:42:09 +0600

    sort method apply in all-review page

--- src/Pages/AllReview/AllReview.jsx
+++ src/Pages/AllReview/AllReview.jsx
@@ -2,27 +2,63 @@ import { useLoaderData } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Review from "../Review/Review";
 import Footer from "../../components/Footer/Footer";
-
+import { useState } from "react";
 
 const AllReview = () => {
-    const allReview = useLoaderData()
+  const allReview = useLoaderData();
+  const [reviews, setReviews] = useState(allReview);
+  const [btnTextShow, setBtnTextShow] = useState("Click");
 
-    return (
-        <div>
-            <Header></Header>
+  const handleRating = (btnText) => {
+    setBtnTextShow("Rating");
+    {
+      btnText === "rating" &&
+        setReviews(reviews.sort((a, b) => b.rating - a.rating));
+    }
+  };
+  const handleYear = (btnText) => {
+    setBtnTextShow("Year");
 
-            <h1 className="text-center text-4xl font-bold mt-7">All <span className="text-[#00D283]">Review</span></h1>
+    {
+      btnText === "year" && setReviews(reviews.sort((a, b) => b.year - a.year));
+    }
+  };
 
-            <div className="w-[280px] md:w-2xl lg:w-4xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-16 my-10">
-                {
-                    allReview.map(review => <Review key={review._id} review={review}></Review>)
-                }
-            </div>
+  return (
+    <div>
+      <Header></Header>
 
-            <Footer></Footer>
-           
+      <div className="w-[280px] md:w-2xl lg:w-4xl mx-auto">
+        <h1 className="text-center text-4xl font-bold mt-7">
+          All <span className="text-[#00D283]">Review</span>
+        </h1>
+
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">
+            {btnTextShow}
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100/50 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a className="font-bold" onClick={() => handleRating("rating")}>Rating</a>
+            </li>
+            <li>
+              <a className="font-bold" onClick={() => handleYear("year")}>Year</a>
+            </li>
+          </ul>
         </div>
-    );
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-16 my-10">
+          {reviews.map((review) => (
+            <Review key={review._id} review={review}></Review>
+          ))}
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default AllReview;
--- src/Pages/Review/Review.jsx
+++ src/Pages/Review/Review.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 const Review = ({ review }) => {
-  const {_id, photo, title, rating} = review
+  const {_id, photo, title, rating, year} = review;
+
   return (
     <div>
       <div className="card bg-base-100 image-full shadow-sm h-72">
@@ -12,10 +13,11 @@ const Review = ({ review }) => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{title}</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <p className="font-bold">
            Rating: {rating}
           </p>
+          <p className="-mt-24">Published: {year}</p>
           <div className="card-actions justify-end">
           <Link to={`/reviewDetails/${_id}`}><button className="btn bg-[#00D283] border-0">Explore Details</button></Link>
           </div>
